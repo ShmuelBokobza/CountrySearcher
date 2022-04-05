f812d10deb53da36e973399562cc6524d40a3713
Shmuel BOKOBZA <samuel.bokobza@gmail.com>
2022-04-05 11:39:13 +0300

    First and Final Commit

---
+++ src/components/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <div className='logo'>
+            <img src="./logo.jpg" alt="Logo App" />
+            <h3>Country Map</h3>
+            
+        </div>
+    );
+};
+
+export default Logo;
---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo/>
+            <Navigation/>
+            <h1>About</h1>      
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore itaque earum porro ut voluptatibus totam, excepturi facere ratione alias repellendus voluptatem quae voluptas assumenda repudiandae illum. Natus, ex. Eius, laborum?  </p>
+      
+        </div>
+    );
+};
+
+export default About;
